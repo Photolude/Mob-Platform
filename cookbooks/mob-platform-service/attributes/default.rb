e4a0901e46da73f439493e95532e9bae5b697d8a
Nikody Keating <nkeating@photolude.com>
2013-11-30 13:19:40 -0800

    - Adding the ability to deploy the mob-platform-service leveraging Chef.  More updates are coming to enable the build system to be able to leverage versioning to generate new chef cookbooks during build time. - Separating configurable settings from spring configurations in order to support Chef templates with the lest amount of duplicate data - Changed all mob versions to the same version number

---
+++ cookbooks/mob-platform-service/attributes/default.rb
@@ -0,0 +1,7 @@
+default["tomcat"]["webapp_dir"] = "/tomcat/webapps"
+default["repo"]["mob-platform-service"] = "http://v-server:9000/job/The%20Mob%20Platform/lastSuccessfulBuild/artifact/PlatformService/target/mob-platform-service-1.0.1.war"
+
+default["mob"]["db"]["driver"] = "com.mysql.jdbc.Driver"
+default["mob"]["db"]["url"] = "jdbc:mysql://localhost/photolude"
+default["mob"]["db"]["user"] = "serviceuser"
+default["mob"]["db"]["password"] = "password"
---
+++ cookbooks/mob-platform-service/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: mob-platform-service
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+cookbook_file "mob-platform-service-1.0.1.war" do
+	path node["tomcat"]["webapp_dir"] + "/mob-platform-service.war"
+	action :create_if_missing
+end
+
+template "config.properties" do
+	path node["tomcat"]["webapp_dir"] + "/mob-platform-service/WEB-INF/config.properties"
+	source "config.properties"
+	action :create
+	notifies :restart, "service[tomcat]"
+end
+

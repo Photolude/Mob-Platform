d1900bf340c4b1647d962d493f55edf36542ea78
Nikody Keating <nkeating@photolude.com>
2013-11-30 22:03:06 -0800

    Fixing default setting to be only a url

--- cookbooks/mob-platform-service/attributes/default.rb
+++ cookbooks/mob-platform-service/attributes/default.rb
@@ -5,4 +5,4 @@ default["mob"]["db"]["url"] = "jdbc:mysql://localhost/photolude"
 default["mob"]["db"]["user"] = "serviceuser"
 default["mob"]["db"]["password"] = "password"
 
-default["mob"]["services"]["access"] = "mob.services.access=http://localhost:13051/ceem"
+default["mob"]["services"]["access"] = "http://localhost:13051/ceem"

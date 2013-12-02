9b01d5bae0b02a60a6b24a621e3f8b6c838fc01b
Nikody Keating <nkeating@photolude.com>
2013-12-01 22:02:22 -0800

    Adding changes to support files being put into cookbooks in their final named form.  This reduces logic at deploy time.

--- assembleAndUploadRecipes.rb
+++ assembleAndUploadRecipes.rb
@@ -10,17 +10,16 @@ def copyFiles(srcMask, dest)
 	FileUtils.mkpath(dest)
 	
 	Dir.glob(srcMask) do |filePath|
-		fileName = File.basename(filePath)
 		outdata = File.read(filePath)
 
-		File.open(dest + fileName, 'w') do |out|
+		File.open(dest, 'w') do |out|
 			out << outdata
 		end
 	end
 end
 
-copyFiles("PlatformService/target/mob-platform-service-*.war", "cookbooks/mob-platform-service/files/default/")
-copyFiles("PlatformWebsite/target/mob-platform-website-*.war", "cookbooks/mob-platform-website/files/default/")
+copyFiles("PlatformService/target/mob-platform-service-*.war", "cookbooks/mob-platform-service/files/default/mob-platform-service.war")
+copyFiles("PlatformWebsite/target/mob-platform-website-*.war", "cookbooks/mob-platform-website/files/default/mob-platform-website.war")
 
 system "knife cookbook upload mob-platform-service"
 system "knife cookbook upload mob-platform-website"

bb9c3406c07737bea5762f75dc0aa63d53e2325c
Nikody Keating <nkeating@photolude.com>
2013-12-01 22:28:18 -0800

    Reducing required dependencies

--- cookbooks/mob-platform-website/recipes/default.rb
+++ cookbooks/mob-platform-website/recipes/default.rb
@@ -6,13 +6,6 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-require 'chef/cookbook/metadata'
-
-metadata_file = ARGV.first || 'metadata.rb'
-
-# read in metadata
-metadata = Chef::Cookbook::Metadata.new
-metadata.from_file(metadata_file)
 
 cookbook_file "mob-platform-website.war" do
 	path node["tomcat"]["webapp_dir"] + "/mob-platform-website.war"

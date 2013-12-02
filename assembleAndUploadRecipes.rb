b07f3d68d7718f23191e25adf656f69a2a0be71d
Nikody Keating <nkeating@photolude.com>
2013-12-01 21:09:38 -0800

    adding ruby script to move files and upload chef recipes as part of the build system.

---
+++ assembleAndUploadRecipes.rb
@@ -0,0 +1,23 @@
+#
+# This script assembles and uploads the chef cookbooks for the build system
+#
+# Copyright 2013, Photolude, LLC
+#
+require 'pathname'
+
+def copyFiles(srcMask, dest)
+	Dir.glob(srcMask) do |filePath|
+		fileName = File.basename(filePath)
+		outdata = File.read(filePath)
+
+		File.open(dest + fileName, 'w') do |out|
+			out << outdata
+		end
+	end
+end
+
+copyFiles("PlatformService/target/mob-platform-service-*.war", "cookbooks/mob-platform-service/files/default/")
+copyFiles("PlatformWebsite/target/mob-platform-website-*.war", "cookbooks/mob-platform-website/files/default/")
+
+system "knife cookbook upload mob-platform-service"
+system "knife cookbook upload mob-platform-website"

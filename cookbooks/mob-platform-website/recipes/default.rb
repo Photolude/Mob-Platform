910cb519d4e15ccfc7d58fd116333aef0dacaf4e
Nikody Keating <nkeating@photolude.com>
2013-12-13 20:51:46 -0800

    Removing syntax error

--- cookbooks/mob-platform-website/recipes/default.rb
+++ cookbooks/mob-platform-website/recipes/default.rb
@@ -8,18 +8,17 @@
 #
 
 # Deploy the new war file
-
 cookbook_file "mob-platform-website.war" do
 	path 
 	action :create
-	notify: :restart, "service[tomcat7]"
+	notify :restart, "service[tomcat7]"
 end
 
 # Deploy the new version file
 cookbook_file "version.txt" do
 	path node["tomcat"]["webapp_dir"] + "/mob-platform-website/WEB-INF/version.txt"
 	action :create
-	notify: :restart, "service[tomcat7]"
+	notify :restart, "service[tomcat7]"
 end
 
 # Setup the configuration
@@ -27,5 +26,5 @@ template "config.properties" do
 	path node["tomcat"]["webapp_dir"] + "/mob-platform-website/WEB-INF/config.properties"
 	source "config.properties"
 	action :create
-	notify: :restart, "service[tomcat7]"
+	notify :restart, "service[tomcat7]"
 end

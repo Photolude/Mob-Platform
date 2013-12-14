c681040ac0b5a8cb813cc8ef6aa93fffbd9f076a
Nikody Keating <nkeating@photolude.com>
2013-12-13 21:10:12 -0800

    Made a change to declare a tomcat service

--- cookbooks/mob-platform-website/recipes/default.rb
+++ cookbooks/mob-platform-website/recipes/default.rb
@@ -7,18 +7,22 @@
 # The rights for this file fall under the same rights as the git repository containing it
 #
 
+service "tomcat7" do
+	supports :restart => true, :status => true
+end
+
 # Deploy the new war file
 cookbook_file "mob-platform-website.war" do
 	path 
 	action :create
-	notifies :restart, "service[tomcat7]"
+	notifies :restart, "service[tomcat7]", :immediately
 end
 
 # Deploy the new version file
 cookbook_file "version.txt" do
 	path node["tomcat"]["webapp_dir"] + "/mob-platform-website/WEB-INF/version.txt"
 	action :create
-	notifies :restart, "service[tomcat7]"
+	notifies :restart, "service[tomcat7]", :immediately
 end
 
 # Setup the configuration
@@ -26,5 +30,5 @@ template "config.properties" do
 	path node["tomcat"]["webapp_dir"] + "/mob-platform-website/WEB-INF/config.properties"
 	source "config.properties"
 	action :create
-	notifies :restart, "service[tomcat7]"
+	notifies :restart, "service[tomcat7]", :immediately
 end
